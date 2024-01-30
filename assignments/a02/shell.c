7ee85c0990e3df41100529c485673423cbe4b164
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-01-30 00:35:30 -0500

    Add assignment 2 boilerplate

---
+++ assignments/a02/shell.c
@@ -0,0 +1,14 @@
+/**
+--------------------------------------
+Author: Aidan Traboulay
+ID: 200115590
+Email: trab5590@mylaurier.ca
+--------------------------------------
+**/
+
+#include <stdio.h>
+
+int main() {
+
+    return 0;
+}
